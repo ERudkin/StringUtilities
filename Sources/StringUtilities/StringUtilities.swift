bab0661c54cd31b3ab66016c14702c6f26a0e869
Elliot Rudkin <Elliot@letswork.io>
2025-06-23 17:18:04 +0200

    initial commit - StringUtilities

--- Sources/StringUtilities/StringUtilities.swift
+++ Sources/StringUtilities/StringUtilities.swift
@@ -1,2 +1,23 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct StringUtilities {
+    public static func isPalindrome(_ text: String) -> Bool {
+        let cleaned = text.lowercased().filter { $0.isLetter }
+        return cleaned == String(cleaned.reversed())
+    }
+    
+    public static func wordCount(_ text: String) -> Int {
+        let words = text.split { $0.isWhitespace || $0.isNewline }
+        return words.count
+    }
+    
+    public static func reversedWords(_ text: String) -> String {
+        let words = text.split(separator: " ").reversed()
+        return words.joined(separator: " ")
+    }
+    
+    public static func removeWhitespace(_ text: String) -> String {
+        return text.filter { !$0.isWhitespace && !$0.isNewline }
+    }
+}
--- Tests/StringUtilitiesTests/StringUtilitiesTests.swift
+++ Tests/StringUtilitiesTests/StringUtilitiesTests.swift
@@ -2,11 +2,23 @@ import XCTest
 @testable import StringUtilities
 
 final class StringUtilitiesTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testIsPalindrome() {
+        XCTAssertTrue(StringUtilities.isPalindrome("Racecar"))
+        XCTAssertTrue(StringUtilities.isPalindrome("A man a plan a canal Panama"))
+        XCTAssertFalse(StringUtilities.isPalindrome("Hello World"))
+    }
+    
+    func testWordCount() {
+        XCTAssertEqual(StringUtilities.wordCount("Swift is great"), 3)
+        XCTAssertEqual(StringUtilities.wordCount("  Hello   World  "), 2)
+    }
+    
+    func testReversedWords() {
+        XCTAssertEqual(StringUtilities.reversedWords("one two three"), "three two one")
+    }
+    
+    func testRemoveWhitespace() {
+        XCTAssertEqual(StringUtilities.removeWhitespace(" a b c "), "abc")
     }
 }
